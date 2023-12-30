2e1352c6b7f1282f305e7686734814ed2403f392
Hos Es <hosteam01@gmail.com>
2023-12-30 16:18:43 +0330

    new structure for handling io

--- Book.py
+++ Book.py
@@ -1,12 +1,14 @@
 class Book():
-    def __init__(self, user_name, password, id_number):
-        self.user_name = user_name
-        self.password = password
-        self.id_number = id_number
+    def __init__(self, arg_list):
+        self.user_name = arg_list["name"]
+        self.password = arg_list["password"]
+        self.id_number = arg_list["id"]
     def Show_Column(self):
-        return {"id": self.id_number,
-                "passowd": self.password,
-                "name": self.user_name}
+        return {
+            "id": self.id_number,
+            "passowd": self.password,
+            "name": self.user_name,
+        }
     def Add_Column(self, visa_mode):
         return visa_mode
     def Rem_Column(self, visa_mode):
--- main.py
+++ main.py
@@ -2,9 +2,19 @@
 
 from Book import Book as b
 
-book_1 = b("Hos", "xyz", "123")
+book_1 = b({
+    "name": "Hos",
+    "password": "xyz",
+    "id": "123",
+})
 
 temp = book_1.Show_Column()
 
 for key in temp:
     print(key, temp[key])
+
+print("-"*10)
+print(book_1.Add_Column("Add_Column"))
+
+print("-"*10)
+print(book_1.Rem_Column("Rem_Column"))
